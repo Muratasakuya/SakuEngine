f059f89fa5c988499d12d9a1d3d4945a6f9d7e22
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2025-10-30 02:12:47 +0900

    地割れエフェクトの作成

--- Project/Engine/Effect/Particle/ParticleConfig.h
+++ Project/Engine/Effect/Particle/ParticleConfig.h
@@ -5,7 +5,7 @@
 //============================================================================
 
 static const int kMaxGPUParticles = 0xffff;
-static const int kMaxCPUParticles = 0x3000;
+static const int kMaxCPUParticles = 0x0400;
 static const int kMaxTrailParticles = 64;
 static const int THREAD_EMIT_GROUP = 1024;
 static const int THREAD_UPDATE_GROUP = 1024;
